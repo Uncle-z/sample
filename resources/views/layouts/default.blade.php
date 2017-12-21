0af12acd25c8e2ebacb1bc4c0957f559b677646d
Yinping zou <916103108@qq.com>
2017-12-21 13:08:15 +0000

    finish user signup

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -8,8 +8,9 @@
 <body>
   @include('layouts._header')
 <div class="container">
+	@include('shared._messages')
 	@yield('content')
-  @include('layouts._footer')
+  	@include('layouts._footer')
 </div>
 </body>
 </html>
