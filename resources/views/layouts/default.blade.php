1992e2e9b67d222758a00f70969a3353e649b67b
Yinping zou <916103108@qq.com>
2017-12-28 06:27:01 +0000

    finish login add logout

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -7,10 +7,11 @@
 </head>
 <body>
   @include('layouts._header')
-<div class="container">
+<div class="container" id="app">
 	@include('shared._messages')
 	@yield('content')
   	@include('layouts._footer')
 </div>
+<script src="/js/app.js"></script>
 </body>
 </html>
