d3c7605d91d35423d44ad6c67850f5670dce3d4f
Yinping zou <916103108@qq.com>
2017-12-30 06:47:29 +0000

    add account activetions

--- app/Models/User.php
+++ app/Models/User.php
@@ -33,6 +33,12 @@ class User extends Authenticatable
         'password', 'remember_token',
     ];
     
+    public static function boot(){
+        parent::boot();
+        static::creating(function($user){
+            $user->activation_token = str_random(30);
+        });
+    }
     /**
      * [gravatar 获取用户在gravatar上的头像]
      * @param  string $size [description]
